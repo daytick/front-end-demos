568a760d3a1c3eaf0cf78c63ddfa7f0865836219
daytick <luyi95@foxmail.com>
2021-06-05 20:31:43 +0800

    cross-domain: encapsulate function jsonp()

--- cross-domain/frank/public/frank.js
+++ cross-domain/frank/public/frank.js
@@ -1,11 +1,20 @@
-const rand = 'JSONP_frank.com_' + Math.random()
-window[rand] = (data) => {
-  console.log(data)
+function jsonp(url) {
+  return new Promise((resolve, reject) => {
+    const rand = 'JSONP_frank.com_' + Math.random()
+    window[rand] = data => {
+      resolve(data)
+    }
+    const script = document.createElement('script')
+    script.src = `${url}?callback=${rand}`
+    script.onload = () => {
+      script.remove()
+    }
+    script.onerror = () => {
+      reject()
+    }
+    document.body.append(script)
+  })
 }
 
-const script = document.createElement('script')
-script.src = `http://qq.com:8888/friends.js?callback=${rand}`
-script.onload = () =>{
-  script.remove()
-}
-document.body.append(script)
+jsonp('http://qq.com:8888/friends.js')
+  .then(data => console.log(data))
